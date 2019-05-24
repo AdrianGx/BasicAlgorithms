e674d4bed32600af9763a86aa4c528dbfcf35945
Adrian Gorea <adrianp.gorea@gmail.com>
2019-05-24 13:31:46 +0300

    Add files via upload

---
+++ Text/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Basic_Algorithms
+{
+     class Program
+    {
+         static void fizzBuzz(int number)
+        {
+            for(int i=0; i<=number; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 == 0))
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else
+                    Console.WriteLine(i);
+
+            }
+        }
+     
+        static void Main(string[] args)
+        {
+                    ### TEST ###
+            Console.Write("Please insert range:");
+            int range = Convert.ToInt32(Console.ReadLine());
+            fizzBuzz(range);
+        }
+    }
+}
